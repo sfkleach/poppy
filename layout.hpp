0f09206b7d67a1ab8fb77ed958afbf24e6e55049
Stephen Leach <sfkleach@gmail.com>
2024-06-23 15:51:03 +0100

    Added a new slot to Procedure layout

--- layout.hpp
+++ layout.hpp
@@ -3,10 +3,11 @@
 
 class ProcedureLayout {
 public:
-    static const unsigned int HeaderSize = 3;
+    static const unsigned int HeaderSize = 4;
     static const int LengthOffset = -1;
-    static const int NumLocalsOffset = 1;
-    static const int InstructionsOffset = 2;
+    static const int GCPointersOffset = 1;
+    static const int NumLocalsOffset = 2;
+    static const int InstructionsOffset = 3;
 };
 
 #endif
